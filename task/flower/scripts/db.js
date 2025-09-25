18e6b3ad7eb650c40a8bc05747f88c304836f358
kimsuyeon1231 <a01099186626@gmail.com>
2025-09-25 12:49:44 +0900

    탄생화찾기 js+jq

---
+++ task/flower/scripts/db.js
@@ -0,0 +1,63 @@
+const birthday_flower = [
+    {
+        month:1,
+        flower:'수선화',
+        content:'자기애, 자존심, 외로움',
+        src:"https://i.pinimg.com/736x/64/c2/80/64c2804574cad8ae2fcc8495e21df56d.jpg",
+    },{
+        month:2,
+        flower:'제비꽃',
+        content:'겸손, 양보',
+        src:"https://i.pinimg.com/1200x/fd/96/5b/fd965b51803d4ab270703232717b24e3.jpg",
+    },{
+        month:3,
+        flower:'수선화',
+        content:'자기애, 자존심, 외로움',
+        src:"https://i.pinimg.com/736x/64/c2/80/64c2804574cad8ae2fcc8495e21df56d.jpg",
+    },{
+        month:4,
+        flower:'스위트피',
+        content:'추억, 즐거움',
+        src:"https://i.pinimg.com/736x/d2/9a/3f/d29a3f79c657a83e4a7c2fde027e9ff9.jpg",
+    },{
+        month:5,
+        flower:'은방울꽃',
+        content:'희망, 섬세함',
+        src:"https://i.pinimg.com/736x/8b/5f/d9/8b5fd93d199d0efe23f23f1508e382de.jpg",
+    },{
+        month:6,
+        flower:'백합',
+        content:'순결',
+        src:"https://i.pinimg.com/736x/fd/fd/ba/fdfdba749f2d60393e04bf082dde5f67.jpg",
+    },{
+        month:7,
+        flower:'미나리아재비',
+        content:'아름다움, 인격',
+        src:"https://i.pinimg.com/736x/98/a8/65/98a86508fe3cdb4b73f61a2109780252.jpg",
+    },{
+        month:8,
+        flower:'글라디올러스',
+        content:'비밀, 상상, 견고함',
+        src:"https://i.pinimg.com/736x/76/cc/f5/76ccf584f527bf6b017b18b98060a624.jpg",
+    },{
+        month:9,
+        flower:'물망초',
+        content:'진실한 사랑',
+        src:"https://i.pinimg.com/736x/af/94/55/af9455a2d6932dbaf19ae47594cacf94.jpg",
+    },{
+        month:10,
+        flower:'금잔화',
+        content:'실망, 비애',
+        src:"https://i.pinimg.com/1200x/45/c4/ea/45c4ea75853d7671dd60a5344177a558.jpg",
+    },{
+        month:11,
+        flower:'국화',
+        content:'성실, 진실',
+        src:"https://i.pinimg.com/736x/95/75/ca/9575ca0002f4a5f18f38947623a7a5e5.jpg",
+    },{
+        month:12,
+        flower:'포인세티아',
+        content:'축하, 감사',
+        src:"https://i.pinimg.com/1200x/73/d2/24/73d22484d0ad54be94b131080728ccd7.jpg",
+    }
+]
---
+++ task/flower/scripts/index.js
@@ -0,0 +1,36 @@
+//jquery ver
+//초기세팅 : 결과숨기기, 공유버튼 숨기기
+const $resultFlower = $('.result_flower')
+const $shareBtn = $('#share_btn')
+
+$resultFlower.hide()
+$shareBtn.hide()
+//'만나기' 버튼 클릭 시 입력한 년/월/일 중 '월' 인식해서 해당 값과 동일한 탄생화 배열 출력하기
+const $resultBtn = $('#result_btn')
+const $year = $('#year')
+const $month = $('#month')
+const $day = $('#day')
+const $flowerImg = $('.flower')
+
+$resultBtn.click(function(){
+    //조건 ? 참 : 거짓;
+    console.log(Boolean($year.val()))
+    const ymd = $year.val() && $month.val() && $day.val() 
+    ymd ? flowerFunc() : 
+    alert('생년월일을 모두 입력해주세요')
+    function flowerFunc(){
+        const $userMonth = $month.val();
+        const resultFlower = birthday_flower[$userMonth-1].flower
+        const resultFlowerLang =  birthday_flower[$userMonth-1].content
+        const resultFlowerSrc =  birthday_flower[$userMonth-1].src
+        $resultFlower.show() // 결과 꽃말 보이기
+        $year.hide()//년 숨기기
+        $month.hide()//월 숨기기
+        $day.hide()//일 숨기기
+        $shareBtn.show()//공유 버튼 보이기
+        $resultBtn.hide()//결과 버튼 숨기기
+        $resultFlower.find('em').text(resultFlower)
+        $resultFlower.find('span').text(resultFlowerLang)
+        $flowerImg.attr('src',resultFlowerSrc)
+    }
+})
